eea7541b5833e91d0161f1ad10401cac1a70c434
JPd3v <>
2022-07-03 01:24:24 -0300

    add pub/sub function
    added function createNewTask which publishes the information of the
    form to all subscribers

--- src/domManipulation.js
+++ src/domManipulation.js
@@ -1,95 +1,84 @@
+import { pubsub } from "./pubSub"
 
 let mainContainer = document.querySelector(".main-container")
 
-function addNewTasks(){
+function addNewTasks() {
     deleteButton()
     createTaskForm()
 }
 
-
-function createNewButton(){
+function createNewButton() {
     let addButton = document.createElement("button")
     addButton.classList.add("btn-add-todo")
     addButton.textContent = "+ add a new task"
-    addButton.addEventListener("click",addNewTasks)
+    addButton.addEventListener("click", addNewTasks)
     mainContainer.appendChild(addButton)
 }
 
-function deleteButton(){
+function deleteButton() {
     let addButton = document.querySelector(".btn-add-todo")
     addButton.remove()
 }
 
-function createTaskForm(){
-    let divContainer= document.createElement("div")
-    let form = document.createElement("form")
+function createTaskForm() {
+    let divContainer = document.createElement("div")
     let title = document.createElement("input")
     let description = document.createElement("input")
     let dueDate = document.createElement("input")
-    let priorityHighLabel = document.createElement("label")   
-    let priorityMediumLabel = document.createElement("label")
-    let priorityLowLabel = document.createElement("label")
-    let priorityHigh = document.createElement("input")
-    let priorityMedium = document.createElement("input")
-    let priorityLow = document.createElement("input")
     let buttonCreateTask = document.createElement("button")
     let buttonCancel = document.createElement("button")
 
     divContainer.classList.add("new-task-form")
-    
-    
 
-    title.placeholder="Title"
-    description.placeholder="Description"
-    dueDate.type="date"
+    title.placeholder = "Title"
+    description.placeholder = "Description"
+    dueDate.type = "date"
 
-    priorityHighLabel.textContent="high"
-    priorityHigh.name="priority"
-    priorityHigh.type="radio"
-    priorityHigh.value="high"
-
-    priorityMediumLabel.textContent="medium"
-    priorityMedium.name="priority"
-    priorityMedium.type="radio"
-    priorityMedium.value="medium"
-
-    priorityLowLabel.textContent="low"
-    priorityLow.type="radio"
-    priorityLow.name="priority"
-    priorityLow.value="low"
-
-    buttonCreateTask.addEventListener("click",()=>{console.log("asd")} )
-    buttonCreateTask.textContent="Create New Task"
-    buttonCancel.addEventListener("click",cancelTaskForm)
-    buttonCancel.textContent="Cancel"
+    buttonCreateTask.addEventListener("click",() =>{
+        createNewTask(title.value, description.value, dueDate.value)
+    })
+    buttonCreateTask.classList.add("create-task")
+    buttonCreateTask.textContent = "Create New Task"
+    buttonCancel.addEventListener("click", cancelTaskForm)
+    buttonCancel.textContent = "Cancel"
 
     mainContainer.appendChild(divContainer)
-    divContainer.appendChild(form)
-    form.appendChild(title)
-    form.appendChild(description)
-    form.appendChild(dueDate)
-    form.appendChild(priorityHighLabel)
-    form.appendChild(priorityMediumLabel)
-    form.appendChild(priorityLowLabel)
-    priorityHighLabel.appendChild(priorityHigh)
-    priorityMediumLabel.appendChild(priorityMedium)
-    priorityLowLabel.appendChild(priorityLow)
-    form.appendChild(buttonCreateTask)
-    form.appendChild(buttonCancel)
+    divContainer.appendChild(title)
+    divContainer.appendChild(description)
+    divContainer.appendChild(dueDate)
+    divContainer.appendChild(buttonCreateTask)
+    divContainer.appendChild(buttonCancel)
 }
 
-function deleteTaskForm(){
-    let form= document.querySelector(".new-task-form")
+function deleteTaskForm() {
+    let form = document.querySelector(".new-task-form")
     form.remove()
 }
 
-function cancelTaskForm(){
+function cancelTaskForm() {
     deleteTaskForm()
     createNewButton()
 }
 
+function createNewTask(title,description,dueDate) {
+    pubsub.publish("createNewTask", submitForm(title,description,dueDate))
+    deleteTaskForm()
+    createNewButton()
+}
+
+function submitForm(title, description, dueDate) {
+    return { title, description, dueDate }
+}
+
+pubsub.subscribe("taskListAdd", rederTaskList)
+function rederTaskList(array) {
+    console.log(array)
+    for (const element in array) {
+        console.log(element.title)
+    }
+}
 
 export {
-    createNewButton,
-    addNewTasks
+    createNewButton
 }
+
